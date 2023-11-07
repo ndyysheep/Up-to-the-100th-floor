4a286199d7435094493c3c63724eadfcb3d9a136
ndyysheep <1204623560@qq.com>
2023-11-07 15:16:22 +0800

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	table_class.py
    #	test.py

--- table_class.py
+++ table_class.py
@@ -1,22 +1,24 @@
-<<<<<<<<< Temporary merge branch 1
-
-
-def test():
-    print("Hello world")
-
-=========
 import psycopg2
 
 def selectAll():
-    con = psycopg2.connect(database="db",user="ztq",password="ztqnb123",host="47.113.185.205",port="5432")
-    cursor = con.cursor()
-    cursor.execute('''
-        select *
-        from groupx;
-        ''')
-    rows= cursor.fetchall()
-    for row in rows:
-        print(row)
+    try:
+        con = psycopg2.connect(database="db", user="ztq", password="ztqnb123", host="47.113.185.205", port="5432")
+        cursor = con.cursor()
+        cursor.execute('''
+            SELECT *
+            FROM groupx;
+            ''')
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+    except psycopg2.Error as e:
+        print("Error connecting to the database:", e)
+    finally:
+        if con:
+            con.close()
+
 def test():
     print("Hello sql")
->>>>>>>>> Temporary merge branch 2
+
+# 调用 selectAll 函数来执行 SQL 查询
+selectAll()
